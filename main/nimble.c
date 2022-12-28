cf7ea8ce11ce94e3049ca7127e45219448865fcc
Zeni241 <Zeni241@gmail.com>
2022-12-28 17:20:32 +0500

    Changed project name
    Changed project name in cMakeKists.txt from bleprph to nimble-esp32

--- main/nimble.c
+++ main/nimble.c
@@ -9,69 +9,62 @@
 #include "nimble/nimble_port.h"
 #include "nimble/nimble_port_freertos.h"
 #include "host/ble_hs.h"
-#include "host/ble_uuid.h"
 #include "host/util/util.h"
+#include "host/ble_uuid.h"
 #include "console/console.h"
 #include "services/gap/ble_svc_gap.h"
 #include "services/gatt/ble_svc_gatt.h"
 #include "bleprph.h"
-static const char *tag = "NimBLE_BLE_PRPH";
+#include "nimble.h"
+
+esp_err_t ret;
+static const char *tag = "NimBLE_BLE";
 static uint8_t own_addr_type;
 uint16_t notification_handle;
 uint16_t conn_handle; 
-//!! Comments added by me start with "//!!"
+bool notify_state; //!! When client subscribe to notifications, the value is set to 1.Check this value before sending notifictions.  
+TaskHandle_t xHandle = NULL;
+char *notice; 
+
+
 
 //!! b2bbc642-46da-11ed-b878-0242ac120002
 static const ble_uuid128_t gatt_svr_svc_uuid =
     BLE_UUID128_INIT(0x02, 0x00, 0x12, 0xac, 0x42, 0x02, 0x78, 0xb8, 0xed, 0x11, 0xda, 0x46, 0x42, 0xc6, 0xbb, 0xb2);
 
 //!! c9af9c76-46de-11ed-b878-0242ac120002 
-static const ble_uuid128_t gatt_svr_chr_one_uuid =
+static const ble_uuid128_t gatt_svr_chr_uuid =
     BLE_UUID128_INIT(0x02, 0x00, 0x12, 0xac, 0x42, 0x02, 0x78, 0xb8, 0xed, 0x11, 0xde, 0x46, 0x76, 0x9c, 0xaf, 0xc9);
 
-//!!94f85bd8-46e5-11ed-b878-0242ac120002 
-static const ble_uuid128_t gatt_svr_chr_two_uuid =
-    BLE_UUID128_INIT(0x02, 0x00, 0x12, 0xac, 0x42, 0x02, 0x78, 0xb8, 0xed, 0x11, 0xe5, 0x46, 0xd8, 0x5b, 0xf8, 0x94);
+char characteristic_value[50]="I am characteristic value"; //!! When client read characteristic, he get this value.
+char characteristic_received_value[500]; //!! When client write to characteristic , he set value of this.
 
-
-char characteristic_one_read_value[50]="I am characteristic One value"; //!! When you read characteristic one, you get this
-char characteristic_two_read_value[50]="I am characteristic Two  value"; //!! When you read characteristic two, you get this
-char characteristic_one_received_value[500]; //!! When you write to characteristic one, you set value of this
-char characteristic_two_received_value[500]; //!! When you write to characteristic two, you set value of this
 uint16_t min_length=1; //!! minimum length the client can write to a characterstic
 uint16_t max_length=700; //!! maximum length the client can write to a characterstic
 
-extern uint16_t notification_handle; //!! Whenever value of this variable is changed, server send that value as notification to client 
-extern uint16_t conn_handle;
+// extern uint16_t notification_handle; //!! Whenever value of this variable is changed, server send that value as notification to client 
+// extern uint16_t conn_handle;
 
+void ble_store_config_init(void);
 
-static int gatt_svr_chr_access_one(uint16_t conn_handle, uint16_t attr_handle,
-                                   struct ble_gatt_access_ctxt *ctxt,
-                                   void *arg); //!! Callback function. When ever characrstic one will be accessed by user, this function will execute
-static int gatt_svr_chr_access_two(uint16_t conn_handle, uint16_t attr_handle,
-                                   struct ble_gatt_access_ctxt *ctxt,
-                                   void *arg); //!! Callback function. When ever characrstic two will be accessed by user, this function will execute
+static int gatt_svr_chr_access(uint16_t conn_handle, uint16_t attr_handle,
+                               struct ble_gatt_access_ctxt *ctxt,
+                               void *arg); //!! Callback function. When ever characrstic one will be accessed by user, this function will execute
 
 static int gatt_svr_chr_write(struct os_mbuf *om, uint16_t min_len, uint16_t max_len, void *dst, uint16_t *len);
 
-
+//& ----- gatt_svr copy
 static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
     {
-        
+
         .type = BLE_GATT_SVC_TYPE_PRIMARY,
         .uuid = &gatt_svr_svc_uuid.u,
         .characteristics = (struct ble_gatt_chr_def[]){{
                                                            /*** //!! Characteristic: Just for client reading/writing example. */
-                                                           .uuid = &gatt_svr_chr_one_uuid.u,                    //!! UUID as given above
-                                                           .access_cb = gatt_svr_chr_access_one,                //!! Callback function. When ever this characrstic will be accessed by user, this function will execute
-                                                           .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_WRITE, //!! flags set permissions. In this case User can read This characterstic and can write to it
-                                                       },
-                                                       {
-                                                           /* //!! Characteristic: For sending notifications. This can be read also */
-                                                           .uuid = &gatt_svr_chr_two_uuid.u,
-                                                           .access_cb = gatt_svr_chr_access_two, //!! Callback function. When ever this characrstic will be accessed by user, this function will execute
+                                                           .uuid = &gatt_svr_chr_uuid.u,     //!! UUID as given above
+                                                           .access_cb = gatt_svr_chr_access, //!! Callback function. When ever this characrstic will be accessed by user, this function will execute
                                                            .val_handle = &notification_handle,
-                                                           .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_NOTIFY, //!! This chrstic can be used only to read and notify the user
+                                                           .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_WRITE | BLE_GATT_CHR_F_NOTIFY, //!! flags set permissions. In this case User can read This characterstic and can write to it
                                                        },
                                                        {
                                                            0, /* No more characteristics in this service. This is necessary */
@@ -83,37 +76,32 @@ static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
     },
 };
 
-static int gatt_svr_chr_access_one(uint16_t conn_handle, uint16_t attr_handle,
-                                   struct ble_gatt_access_ctxt *ctxt,
-                                   void *arg)
+static int gatt_svr_chr_access(uint16_t conn_handle, uint16_t attr_handle,
+                               struct ble_gatt_access_ctxt *ctxt,
+                               void *arg)
 {
-  
-    int rc;
 
-      switch (ctxt->op)
-    {
-    case BLE_GATT_ACCESS_OP_READ_CHR: //!! In case user accessed this characterstic to read its value, bellow lines will execute
-        rc = os_mbuf_append(ctxt->om, &characteristic_one_read_value,
-                            sizeof characteristic_one_read_value);
-        return rc == 0 ? 0 : BLE_ATT_ERR_INSUFFICIENT_RES;
+  int rc;
 
-    case BLE_GATT_ACCESS_OP_WRITE_CHR: //!! In case user accessed this characterstic to write, bellow lines will executed.      
-        rc = gatt_svr_chr_write(ctxt->om, min_length, max_length, &characteristic_one_received_value, NULL); //!! Function "gatt_svr_chr_write" givn bellow will fire.
-        printf("Received=%s\n",characteristic_one_received_value); // Print the received value
-         memset(characteristic_one_received_value, '\0', sizeof characteristic_one_received_value); //!! empty sting for next value to receive
-       
-        return rc;
+  switch (ctxt->op)
+  {
+  case BLE_GATT_ACCESS_OP_READ_CHR: //!! In case user accessed this characterstic to read its value, bellow lines will execute
+    rc = os_mbuf_append(ctxt->om, &characteristic_value,
+                        sizeof characteristic_value);
+    return rc == 0 ? 0 : BLE_ATT_ERR_INSUFFICIENT_RES;
 
+  case BLE_GATT_ACCESS_OP_WRITE_CHR:                                                                 //!! In case user accessed this characterstic to write, bellow lines will executed.
+    rc = gatt_svr_chr_write(ctxt->om, min_length, max_length, &characteristic_received_value, NULL); //!! Function "gatt_svr_chr_write" givn bellow will fire.
+    printf("Received=%s\n", characteristic_received_value);                                          // Print the received value
+    //getValues();
+
+return rc;
     default:
-        assert(0);
-        return BLE_ATT_ERR_UNLIKELY;
-    }
+  assert(0);
+  return BLE_ATT_ERR_UNLIKELY;
 
-    /* Unknown characteristic; the nimble stack should not have called this
-     * function.
-     */
-    assert(0);
-    return BLE_ATT_ERR_UNLIKELY;
+
+  }
 }
 
 static int gatt_svr_chr_write(struct os_mbuf *om, uint16_t min_len, uint16_t max_len, void *dst, uint16_t *len)
@@ -135,18 +123,7 @@ static int gatt_svr_chr_write(struct os_mbuf *om, uint16_t min_len, uint16_t max
 
     return 0;
 }
-static int
-gatt_svr_chr_access_two(uint16_t conn_handle, uint16_t attr_handle,
-                               struct ble_gatt_access_ctxt *ctxt, void *arg) //!! In case user accessed this characterstic to read its value, this function will execute
-{
-     int rc;
-    rc = os_mbuf_append(ctxt->om, &characteristic_two_read_value,
-                            sizeof characteristic_two_read_value);
-        return rc == 0 ? 0 : BLE_ATT_ERR_INSUFFICIENT_RES;
-   
-    assert(0);
-    return BLE_ATT_ERR_UNLIKELY;
-}
+
 
 void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg)
 {
@@ -201,6 +178,32 @@ int gatt_svr_init(void)
 
     return 0;
 }
+
+void sendNotification()
+{
+  int rc;
+  struct os_mbuf *om;
+  
+  if (notify_state) //!! This value is checked so that we don't send notifications if user has not subscribed to our notification handle.
+  {
+    om = ble_hs_mbuf_from_flat(notice, sizeof(notice)); //! Value of variable "notice" will be sent as notification.
+    
+    rc = ble_gattc_notify_custom(conn_handle, notification_handle, om);
+   
+
+    if (rc != 0)
+    {
+      printf("\n error notifying; rc\n");
+    }
+  }
+  else
+  {
+      printf("user not subscribed to notifications.\n");
+     
+  }
+  
+}
+
 //@______copy from main.c begins ________
 
 static void
@@ -485,7 +488,7 @@ void startBLE()
   assert(rc == 0);
 
   /* XXX Need to have template for store */
-  ble_store_config_init();
+  
 
   nimble_port_freertos_init(bleprph_host_task);
   printf("characteristic_value at end of startBLE=%s\n", characteristic_value);
--- main/nimble.h
+++ main/nimble.h
@@ -1,20 +1,13 @@
 #ifndef NIMBLE_H
 #define NIMBLE_H
+
 extern TaskHandle_t xHandle;
-
-//extern TaskHandle_t xHandle;
-
+extern char *notice;
 void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg);
 static int bleprph_gap_event(struct ble_gap_event *event, void *arg);
-//void create_char_value();
+void sendNotification();
 void startBLE();
 void stopBLE();
-
-// void sendNotification();
-// void getValues();
-// extern char *notice;
-// extern bool notify_state;
-
 
 #endif
 
